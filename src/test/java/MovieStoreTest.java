667415a0ccc14d2c76f3d81d4d14a20381ef220f
YPaolaRoa <yuli.roa@mercadolibre.com.co>
2022-12-23 11:02:09 -0500

    ready to test

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -1,6 +1,7 @@
+import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
 
-public class MovieStoreTest {
+public class MovieStoreTest implements WithAssertions {
 
     //Retorna una lista sin resultados cuando no hay ninguna película que contenga la cadena que se busca
     @Test
@@ -11,18 +12,17 @@ public class MovieStoreTest {
     //Agrega una pelicula a la tienda de películas
     @Test
     public void addsAMovieToMovieStore() {
-
     }
 
     //Retorna una película cuando hace match con la búsqueda
     @Test
-    public void findsAMovieWhenTitleIsPartiallyMatch() throws Exception {
+    public void findsAMovieWhenTitleIsPartiallyMatch() {
 
     }
 
     //Si varias películas hacen match con la búsqueda las retorna
     @Test
-    public void findsMoviesWhenTitleIsPartiallyMatch() throws Exception {
+    public void findsMoviesWhenTitleIsPartiallyMatch() {
 
     }
 }
